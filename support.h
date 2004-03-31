589bbc3fa5acf9a1a4f402ecee097d9db9639291
Kalle Wallin <kaw@linux.se>
2004-03-31 21:45:19 +0000

    Added strcasestr prototype.
    git-svn-id: https://svn.musicpd.org/ncmpc/trunk@574 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- support.h
+++ support.h
@@ -1,3 +1,5 @@
+#ifndef SUPPORT_H
+#define SUPPORT_H
 
 #ifdef HAVE_LIBGEN_H
 #include <libgen.h>
@@ -7,20 +9,16 @@
 char *basename(char *path);
 #endif
 
-#ifndef HAVE_STRCASESTR
-char *strcasestr(const char *haystack, const char *needle);
-#endif
-
 #define IS_WHITESPACE(c) (c==' ' || c=='\t' || c=='\r' || c=='\n')
 
 char *trim(char *str);
 char *remove_trailing_slash(char *path);
 char *lowerstr(char *str);
-
+char *strcasestr(const char *haystack, const char *needle);
 
 int charset_init(void);
 int charset_close(void);
 char *utf8_to_locale(char *str);
 char *locale_to_utf8(char *str);
 
-
+#endif 
