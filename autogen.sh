f5b40d903e57eb92c38bb640807806bc7caffdcd
Andreas Obergrusberger <tradiaz@yahoo.de>
2007-03-13 19:08:49 +0000

    zpdated autogen.sh
    git-svn-id: https://svn.musicpd.org/ncmpc/branches/tradiaz@5619 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,28 @@
 #! /bin/sh
 
 # Generate config.h.in
-echo "autoheader..."
-autoheader
-
 echo "touch stamp-h"
 touch stamp-h
-
-# rerun libtoolize
-echo "libtoolize..."
-libtoolize --force
 
 # add aclocal.m4 to current dir
 echo "aclocal..."
 aclocal
 
+# rerun libtoolize
+echo "libtoolize..."
+libtoolize --force
+
 # This generates the configure script from configure.in
 echo "autoconf..."
 autoconf
 
+echo "autoheader..."
+autoheader
+
 # Generate Makefile.in from Makefile.am
 echo "automake..."
-automake
+automake --add-missing
+
 
 # configure
 echo "./configure $*"
