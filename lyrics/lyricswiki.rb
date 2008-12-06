7ab9b9714f8710c4498c3296eed6e8a91eb67c65
Max Kellermann <max@duempel.org>
2008-12-06 12:41:29 +0100

    added missing copyright headers

--- lyrics/lyricswiki.rb
+++ lyrics/lyricswiki.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/env ruby
 #
-# Load lyrics from lyricswiki.org
+#  (c) 2004-2008 The Music Player Daemon Project
+#  http://www.musicpd.org/
 #
-# Author: Max Kellermann <max@duempel.org>
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+#
+# Load lyrics from lyricswiki.org
 #
 
 require 'uri'
