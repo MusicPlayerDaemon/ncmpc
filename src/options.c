2cbf373c8a16fbca00d90da9e697bb3e3da8bbc9
Patrick Hallen <patrick.hallen@rwth-aachen.de>
2009-03-29 18:24:19 +0200

    options.c: Fixed compilation with --enable-mini --enable-debug
    With NCMPC_MINI we have no configuration file, so we can't dump the keys.

--- src/options.c
+++ src/options.c
@@ -274,11 +274,13 @@ handle_option(int c, const char *arg)
 		/* the splash screen was removed */
 		break;
 #ifndef NDEBUG
+#ifndef NCMPC_MINI
 	case 'K': /* --dump-keys */
 		read_configuration();
 		write_key_bindings(stdout, KEYDEF_WRITE_ALL | KEYDEF_COMMENT_ALL);
 		exit(EXIT_SUCCESS);
 		break;
+#endif
 #endif
 	default:
 		fprintf(stderr,"Unknown Option %c = %s\n", c, arg);
