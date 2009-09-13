fd5dd6f0067912bbfddc8db1522a6c7d061b5cad
Max Kellermann <max@duempel.org>
2009-09-13 08:37:26 +0200

    plugin: show plugin error messages on the screen
    Due to a typo, stdout was redirected twice to the pipe, and stderr was
    forgotten.

--- src/plugin.c
+++ src/plugin.c
@@ -222,7 +222,7 @@ start_plugin(struct plugin_cycle *cycle, const char *plugin_path)
 
 	if (pid == 0) {
 		dup2(fds[1], 1);
-		dup2(fds[1], 1);
+		dup2(fds[1], 2);
 		close(fds[0]);
 		close(fds[1]);
 		close(0);
