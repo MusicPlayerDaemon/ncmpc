MusicPlayerDaemon/ncmpc

commit 4345c919f9d9c184f2369e630089323d5fdf7691

Implement a search-format configuration option (Mantis #4007)
This configures the song format for the search window,
and defaults to the value of list-format if not specified.

src/screen_artist.c
@@ -28,6 +28,7 @@
 #include "mpdclient.h"
 #include "screen_browser.h"
 #include "filelist.h"
+#include "options.h"
 
 #include <assert.h>
 #include <string.h>
@@ -301,6 +302,7 @@ static void
 screen_artist_init(WINDOW *w, int cols, int rows)
 {
 	browser.lw = list_window_init(w, cols, rows);
+	browser.song_format = options.list_format;
 	artist = NULL;
 	album = NULL;
 }
@@ -332,9 +334,9 @@ screen_artist_resize(int cols, int rows)
 static void
 paint_artist_callback(WINDOW *w, unsigned i,
 		      gcc_unused unsigned y, unsigned width,
-		      bool selected, void *data)
+		      bool selected, const void *data)
 {
-	GPtrArray *list = data;
+	const GPtrArray *list = data;
 	char *p = utf8_to_locale(g_ptr_array_index(list, i));
 
 	screen_browser_paint_directory(w, width, selected, p);
@@ -350,9 +352,9 @@ paint_artist_callback(WINDOW *w, unsigned i,
 static void
 paint_album_callback(WINDOW *w, unsigned i,
 		     gcc_unused unsigned y, unsigned width,
-		     bool selected, void *data)
+		     bool selected, const void *data)
 {
-	GPtrArray *list = data;
+	const GPtrArray *list = data;
 	const char *p;
 	char *q = NULL;
 

src/screen_browser.c
@@ -378,7 +378,7 @@ browser_handle_mouse_event(struct screen_browser *browser, struct mpdclient *c)
 
 static void
 screen_browser_paint_callback(WINDOW *w, unsigned i, unsigned y,
-			      unsigned width, bool selected, void *data);
+			      unsigned width, bool selected, const void *data);
 
 bool
 browser_cmd(struct screen_browser *browser,
@@ -503,14 +503,15 @@ screen_browser_paint_playlist(WINDOW *w, unsigned width,
 static void
 screen_browser_paint_callback(WINDOW *w, unsigned i,
 			      unsigned y, unsigned width,
-			      bool selected, void *data)
+			      bool selected, const void *data)
 {
-	const struct filelist *fl = (const struct filelist *) data;
+	const struct screen_browser *browser = (const struct screen_browser *) data;
 
-	assert(fl != NULL);
-	assert(i < filelist_length(fl));
+	assert(browser != NULL);
+	assert(browser->filelist != NULL);
+	assert(i < filelist_length(browser->filelist));
 
-	const struct filelist_entry *entry = filelist_get(fl, i);
+	const struct filelist_entry *entry = filelist_get(browser->filelist, i);
 	assert(entry != NULL);
 
 	const struct mpd_entity *entity = entry->entity;
@@ -539,7 +540,7 @@ screen_browser_paint_callback(WINDOW *w, unsigned i,
 
 	case MPD_ENTITY_TYPE_SONG:
 		paint_song_row(w, y, width, selected, highlight,
-			       mpd_entity_get_song(entity), NULL);
+			       mpd_entity_get_song(entity), NULL, browser->song_format);
 		break;
 
 	case MPD_ENTITY_TYPE_PLAYLIST:
@@ -559,5 +560,5 @@ void
 screen_browser_paint(const struct screen_browser *browser)
 {
 	list_window_paint2(browser->lw, screen_browser_paint_callback,
-			   browser->filelist);
+			   browser);
 }

src/screen_queue.c
@@ -504,7 +504,7 @@ screen_queue_title(char *str, size_t size)
 static void
 screen_queue_paint_callback(WINDOW *w, unsigned i,
 			    unsigned y, unsigned width,
-			    bool selected, gcc_unused void *data)
+			    bool selected, gcc_unused const void *data)
 {
 	assert(playlist != NULL);
 	assert(i < playlist_length(playlist));
@@ -519,7 +519,7 @@ screen_queue_paint_callback(WINDOW *w, unsigned i,
 
 	paint_song_row(w, y, width, selected,
 		       (int)mpd_song_get_id(song) == current_song_id,
-		       song, row_hscroll);
+		       song, row_hscroll, options.list_format);
 }
 
 static void

doc/config.sample
@@ -79,6 +79,9 @@
 ## The format used to display songs in the main window.
 #list-format = "%name%|[%artist% - ]%title%|%file%"
 
+## The format used to display songs in the search window.
+#search-format = "%name%|[%artist% - ]%title%|%file%"
+
 ## The format used to display songs on the status line.
 #status-format = "[%artist% - ]%title%|%shortfile%"
 

doc/ncmpc.1
@@ -167,6 +167,9 @@ the separator to show at the end of the scrolling title.
 .B list\-format = SONG FORMAT
 The format used to display songs in the main window.
 .TP 
+.B search\-format = SONG FORMAT
+The format used to display songs in the search window. Default is to use list\-format.
+.TP
 .B status\-format = SONG FORMAT
 The format used to display songs on the status line.
 .TP

src/conf.c
@@ -48,6 +48,7 @@
 #define CONF_COLOR "color"
 #define CONF_COLOR_DEFINITION "colordef"
 #define CONF_LIST_FORMAT "list-format"
+#define CONF_SEARCH_FORMAT "search-format"
 #define CONF_STATUS_FORMAT "status-format"
 #define CONF_XTERM_TITLE_FORMAT "xterm-title-format"
 #define CONF_LIST_WRAP "wrap-around"
@@ -435,6 +436,10 @@ parse_line(char *line)
 	else if (!strcasecmp(CONF_LIST_FORMAT, name)) {
 		g_free(options.list_format);
 		options.list_format = get_format(value);
+		/* search format string */
+	} else if (!strcasecmp(CONF_SEARCH_FORMAT, name)) {
+		g_free(options.search_format);
+		options.search_format = get_format(value);
 		/* status format string */
 	} else if (!strcasecmp(CONF_STATUS_FORMAT, name)) {
 		g_free(options.status_format);

src/list_window.c
@@ -380,7 +380,7 @@ list_window_paint(const struct list_window *lw,
 void
 list_window_paint2(const struct list_window *lw,
 		   list_window_paint_callback_t paint_callback,
-		   void *callback_data)
+		   const void *callback_data)
 {
 	bool show_cursor = !lw->hide_cursor &&
 		(!options.hardware_cursor || lw->range_selection);

src/list_window.h
@@ -35,7 +35,7 @@ typedef void
 (*list_window_paint_callback_t)(WINDOW *w, unsigned i,
 				unsigned y, unsigned width,
 				bool selected,
-				void *data);
+				const void *data);
 
 struct list_window {
 	WINDOW *w;
@@ -94,7 +94,7 @@ void list_window_paint(const struct list_window *lw,
 void
 list_window_paint2(const struct list_window *lw,
 		   list_window_paint_callback_t paint_callback,
-		   void *callback_data);
+		   const void *callback_data);
 
 /* perform basic list window commands (movement) */
 bool

commit e7c6e5fe86fb1660a291eecce441c936f2877f53

screen_browser: fix crash on "jump" (hotkey ".")
Regression caused by commit 4345c919f: screen_browser_paint_callback()
now expects a struct screen_browser pointer instead of a struct
filelist pointer, because it now requires access to the song format.
However, browser_lw_callback() still wants a struct filelist, and the
CMD_LIST_JUMP handler passes the former to screen_jump() for both
callbacks.

src/screen_artist.c
@@ -717,13 +717,15 @@ screen_artist_cmd(struct mpdclient *c, command_t cmd)
 	case CMD_LIST_JUMP:
 		switch (mode) {
 		case LIST_ARTISTS:
-			screen_jump(browser.lw, screen_artist_lw_callback,
+			screen_jump(browser.lw,
+				    screen_artist_lw_callback, artist_list,
 				    paint_artist_callback, artist_list);
 			artist_repaint();
 			return true;
 
 		case LIST_ALBUMS:
-			screen_jump(browser.lw, screen_artist_lw_callback,
+			screen_jump(browser.lw,
+				    screen_artist_lw_callback, album_list,
 				    paint_album_callback, album_list);
 			artist_repaint();
 			return true;

src/screen_browser.c
@@ -403,8 +403,9 @@ browser_cmd(struct screen_browser *browser,
 			    browser->filelist);
 		return true;
 	case CMD_LIST_JUMP:
-		screen_jump(browser->lw, browser_lw_callback,
-			    screen_browser_paint_callback, browser->filelist);
+		screen_jump(browser->lw,
+			    browser_lw_callback, browser->filelist,
+			    screen_browser_paint_callback, browser);
 		return true;
 
 #ifdef HAVE_GETMOUSE

src/screen_find.c
@@ -93,9 +93,8 @@ screen_find(struct list_window *lw, command_t findcmd,
  * which begins with this string while the users types */
 void
 screen_jump(struct list_window *lw,
-	    list_window_callback_fn_t callback_fn,
-	    list_window_paint_callback_t paint_callback,
-	    void *callback_data)
+	    list_window_callback_fn_t callback_fn, void *callback_data,
+	    list_window_paint_callback_t paint_callback, void *paint_data)
 {
 	const int WRLN_MAX_LINE_SIZE = 1024;
 	int key = 65;
@@ -134,7 +133,7 @@ screen_jump(struct list_window *lw,
 
 		/* repaint the list_window */
 		if (paint_callback != NULL)
-			list_window_paint2(lw, paint_callback, callback_data);
+			list_window_paint2(lw, paint_callback, paint_data);
 		else
 			list_window_paint(lw, callback_fn, callback_data);
 		wrefresh(lw->w);

src/screen_find.h
@@ -42,8 +42,7 @@ screen_find(struct list_window *lw,
  * which begins with this string while the users types */
 void
 screen_jump(struct list_window *lw,
-	    list_window_callback_fn_t callback_fn,
-	    list_window_paint_callback_t paint_callback,
-	    void *callback_data);
+	    list_window_callback_fn_t callback_fn, void *callback_data,
+	    list_window_paint_callback_t paint_callback, void *paint_data);
 
 #endif

src/screen_queue.c
@@ -644,7 +644,7 @@ screen_queue_cmd(struct mpdclient *c, command_t cmd)
 		screen_queue_repaint();
 		return true;
 	case CMD_LIST_JUMP:
-		screen_jump(lw, screen_queue_lw_callback, NULL, NULL);
+		screen_jump(lw, screen_queue_lw_callback, NULL, NULL, NULL);
 		screen_queue_save_selection();
 		screen_queue_repaint();
 		return true;