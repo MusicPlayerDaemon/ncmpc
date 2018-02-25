193444b6b47078aaeb261bb0a48f114f166f32dc
Max Kellermann <max@musicpd.org>
2018-02-25 22:30:22 +0100

    colors: disable NamedColor::color if compiled without color support

--- src/colors.cxx
+++ src/colors.cxx
@@ -48,8 +48,15 @@ struct CustomColor {
 
 struct NamedColor {
 	const char *name;
+#ifdef ENABLE_COLORS
 	int color;
+#endif
 	int mono;
+
+#ifndef ENABLE_COLORS
+	constexpr NamedColor(const char *_name, int, int _mono)
+		:name(_name), mono(_mono) {}
+#endif
 };
 
 static NamedColor colors[COLOR_END] = {
