d9c8236f762cc76c11ef7adc15647c93b8108ec5
Max Kellermann <max@musicpd.org>
2018-03-10 15:15:54 +0100

    Compiler.h: add macro for attribute "returns_nonnull"

--- src/Compiler.h
+++ src/Compiler.h
@@ -75,6 +75,7 @@
 
 #define gcc_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
 #define gcc_nonnull_all __attribute__((nonnull))
+#define gcc_returns_nonnull __attribute__((returns_nonnull))
 
 #define gcc_likely(x) __builtin_expect (!!(x), 1)
 #define gcc_unlikely(x) __builtin_expect (!!(x), 0)
@@ -104,6 +105,7 @@
 
 #define gcc_nonnull(...)
 #define gcc_nonnull_all
+#define gcc_returns_nonnull
 
 #define gcc_likely(x) (x)
 #define gcc_unlikely(x) (x)
