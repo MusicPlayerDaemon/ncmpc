MusicPlayerDaemon/ncmpc

commit abf50d76048b1afa950451b76fd3c371354f8c19

wreadln: return std::string

src/wreadln.cxx
@@ -346,7 +346,7 @@ wreadln_delete_char(struct wreadln *wr, size_t x)
 
 /* libcurses version */
 
-static char *
+static std::string
 _wreadln(WINDOW *w,
 	 const char *prompt,
 	 const char *initial_value,
@@ -446,7 +446,7 @@ _wreadln(WINDOW *w,
 			if (history) {
 				history->pop_back();
 			}
-			return nullptr;
+			return {};
 
 		case KEY_LEFT:
 		case KEY_CTRL_B:
@@ -563,7 +563,7 @@ _wreadln(WINDOW *w,
 	return g_strdup(wr.value.c_str());
 }
 
-char *
+std::string
 wreadln(WINDOW *w,
 	const char *prompt,
 	const char *initial_value,
@@ -575,7 +575,7 @@ wreadln(WINDOW *w,
 			 history, completion, false);
 }
 
-char *
+std::string
 wreadln_masked(WINDOW *w,
 	       const char *prompt,
 	       const char *initial_value,

src/callbacks.cxx
@@ -34,12 +34,11 @@ _mpdclient_auth_callback(struct mpdclient *c, unsigned recursion)
 	if (recursion > 2)
 		return false;
 
-	char *password = screen_read_password(nullptr);
-	if (password == nullptr)
+	const auto password = screen_read_password(nullptr);
+	if (password.empty())
 		return false;
 
-	mpd_send_password(connection, password);
-	g_free(password);
+	mpd_send_password(connection, password.c_str());
 
 	mpd_response_finish(connection);
 	mpdclient_update(c);

src/save_playlist.cxx
@@ -91,13 +91,14 @@ playlist_save(struct mpdclient *c, char *name, char *defaultname)
 		PlaylistNameCompletion completion(*c);
 
 		/* query the user for a filename */
-		filename = screen_readln(_("Save queue as"),
-					 defaultname,
-					 nullptr,
-					 &completion);
-		if (filename == nullptr)
+		auto result = screen_readln(_("Save queue as"),
+					    defaultname,
+					    nullptr,
+					    &completion);
+		if (result.empty())
 			return -1;
 
+		filename = g_strdup(result.c_str());
 		filename = g_strstrip(filename);
 	} else
 #endif

src/screen.hxx
@@ -35,6 +35,7 @@
 #include <glib.h>
 
 #include <memory>
+#include <string>
 #include <map>
 
 struct mpdclient;
@@ -94,7 +95,7 @@ private:
 	size_t buf_size;
 
 public:
-	char *findbuf;
+	std::string findbuf;
 	History find_history;
 
 #ifndef NCMPC_MINI

src/screen_chat.cxx
@@ -145,7 +145,7 @@ screen_chat_get_prefix()
 }
 
 static void
-screen_chat_send_message(struct mpdclient *c, char *msg)
+screen_chat_send_message(struct mpdclient *c, const char *msg)
 {
 	char *prefix = screen_chat_get_prefix();
 	char *full_msg = g_strconcat(prefix, LocaleToUtf8(msg).c_str(),
@@ -162,18 +162,16 @@ ChatPage::OnCommand(struct mpdclient &c, command_t cmd)
 		return true;
 
 	if (cmd == CMD_PLAY) {
-		char *message = screen_readln(_("Your message"), nullptr, nullptr, nullptr);
+		auto message = screen_readln(_("Your message"), nullptr, nullptr, nullptr);
 
 		/* the user entered an empty line */
-		if (message == nullptr)
+		if (message.empty())
 			return true;
 
 		if (CheckChatSupport(c))
-			screen_chat_send_message(&c, message);
+			screen_chat_send_message(&c, message.c_str());
 		else
 			screen_status_message(_("Message could not be sent"));
-
-		g_free(message);
 
 		return true;
 	}

src/screen_find.cxx
@@ -46,15 +46,12 @@ screen_find(ScreenManager &screen, ListWindow *lw, command_t findcmd,
 	switch (findcmd) {
 	case CMD_LIST_FIND:
 	case CMD_LIST_RFIND:
-		if (screen.findbuf) {
-			g_free(screen.findbuf);
-			screen.findbuf=nullptr;
-	}
+		screen.findbuf.clear();
 		/* fall through */
 
 	case CMD_LIST_FIND_NEXT:
 	case CMD_LIST_RFIND_NEXT:
-		if (!screen.findbuf) {
+		if (screen.findbuf.empty()) {
 			char *value = options.find_show_last_pattern
 				? (char *) -1 : nullptr;
 			screen.findbuf=screen_readln(prompt,
@@ -63,21 +60,21 @@ screen_find(ScreenManager &screen, ListWindow *lw, command_t findcmd,
 						     nullptr);
 		}
 
-		if (screen.findbuf == nullptr)
+		if (screen.findbuf.empty())
 			return true;
 
 		found = reversed
 			? lw->ReverseFind(callback_fn, callback_data,
-					  screen.findbuf,
+					  screen.findbuf.c_str(),
 					  options.find_wrap,
 					  options.bell_on_wrap)
 			: lw->Find(callback_fn, callback_data,
-				   screen.findbuf,
+				   screen.findbuf.c_str(),
 				   options.find_wrap,
 				   options.bell_on_wrap);
 		if (!found) {
 			screen_status_printf(_("Unable to find \'%s\'"),
-					     screen.findbuf);
+					     screen.findbuf.c_str());
 			screen_bell();
 		}
 		return true;
@@ -135,8 +132,7 @@ screen_jump(ScreenManager &screen, ListWindow *lw,
 		wrefresh(lw->w);
 	}
 
-	g_free(screen.findbuf);
-	screen.findbuf = g_strdup(search_str);
+	screen.findbuf = search_str;
 
 	/* ncmpc should get the command */
 	keyboard_unread(key);

src/screen_init.cxx
@@ -49,7 +49,6 @@ ScreenManager::ScreenManager()
 {
 	buf_size = layout.size.width;
 	buf = (char *)g_malloc(buf_size);
-	findbuf = nullptr;
 
 	if (!options.hardware_cursor)
 		leaveok(main_window.w, true);
@@ -60,7 +59,6 @@ ScreenManager::ScreenManager()
 ScreenManager::~ScreenManager()
 {
 	g_free(buf);
-	g_free(findbuf);
 
 #ifndef NCMPC_MINI
 	if (welcome_source_id != 0)

src/screen_queue.cxx
@@ -300,17 +300,16 @@ handle_add_to_playlist(struct mpdclient *c)
 #endif
 
 	/* get path */
-	char *path = screen_readln(_("Add"),
-				   nullptr,
-				   nullptr,
-				   completion);
+	auto path = screen_readln(_("Add"),
+				  nullptr,
+				  nullptr,
+				  completion);
 
 	/* add the path to the playlist */
-	if (path != nullptr) {
-		mpdclient_cmd_add_path(c, LocaleToUtf8(path).c_str());
+	if (!path.empty()) {
+		mpdclient_cmd_add_path(c, LocaleToUtf8(path.c_str()).c_str());
 	}
 
-	g_free(path);
 	return 0;
 }
 

commit 227a6cdb46ebcd11cd48c2b5cf430a31954e00a3

wreadln: fix memory leak in return
Fallout from commit abf50d76048b1afa950451b76fd3c371354f8c19

src/wreadln.cxx
@@ -553,10 +553,7 @@ _wreadln(WINDOW *w,
 		}
 	}
 
-	if (wr.value.empty())
-		return nullptr;
-
-	return g_strdup(wr.value.c_str());
+	return std::move(wr.value);
 }
 
 std::string