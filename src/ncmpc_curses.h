1150f7931de7588643437069dd798724dc1ceb47
Max Kellermann <max@musicpd.org>
2018-09-24 12:42:00 +0200

    meson.build: use only curses.h, not ncurses.h
    On all ncurses versions I checked, no matter how old, "ncurses.h" is a
    symlink to "curses.h".

--- src/ncmpc_curses.h
+++ src/ncmpc_curses.h
@@ -21,8 +21,6 @@
 #  include <ncursesw/curses.h>
 #elif defined(HAVE_NCURSES_CURSES_H)
 #  include <ncurses/curses.h>
-#elif defined(HAVE_NCURSES_H)
-#  include <ncurses.h>
 #elif defined(HAVE_CURSES_H)
 #  include <curses.h>
 #else
