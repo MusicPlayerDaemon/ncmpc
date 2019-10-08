932536a83421402ad5c6746466e2d90d13b8f6c0
Max Kellermann <max@musicpd.org>
2019-10-08 17:45:57 +0200

    KeyName: support F*, Ctrl-* and Alt-*
    Closes https://github.com/MusicPlayerDaemon/ncmpc/issues/55

--- src/KeyName.cxx
+++ src/KeyName.cxx
@@ -20,6 +20,7 @@
 #include "KeyName.hxx"
 #include "i18n.h"
 #include "util/CharUtil.hxx"
+#include "util/StringCompare.hxx"
 
 #include <curses.h>
 
@@ -39,6 +40,28 @@ ParseKeyName(const char *s) noexcept
 			return std::make_pair(-1, s);
 
 		return std::make_pair(s[1], s + 3);
+	} else if (*s == 'F') {
+		char *end;
+		const auto value = strtoul(s + 1, &end, 0);
+		if (end == s + 1)
+			return std::make_pair(-1, end);
+
+		if (value >= 64)
+			return std::make_pair(-1, s);
+
+		return std::make_pair(KEY_F(value), end);
+	} else if (auto ctrl = StringAfterPrefixIgnoreCase(s, "ctrl-")) {
+		int ch = *ctrl++;
+		if (ch >= 0x40 && ch < 0x60)
+			return std::make_pair(ch - 0x40, ctrl);
+
+		return std::make_pair(-1, s);
+	} else if (auto alt = StringAfterPrefixIgnoreCase(s, "alt-")) {
+		int ch = *alt++;
+		if (ch >= 0x40 && ch < 0x80)
+			return std::make_pair(0xc0 | (ch - 0x40), alt);
+
+		return std::make_pair(-1, s);
 	} else {
 		char *end;
 		const auto value = strtol(s, &end, 0);
@@ -53,6 +76,28 @@ const char *
 GetKeyName(int key) noexcept
 {
 	static char buf[32];
+
+	for (int i = 0; i < 64; ++i) {
+		if (key == KEY_F(i)) {
+			snprintf(buf, sizeof(buf), "F%u", i);
+			return buf;
+		}
+	}
+
+	const char *name = keyname(key);
+	if (name != nullptr) {
+		if (name[0] == '^' && name[1] != 0 && name[2] == 0) {
+			/* translate "^X" to "Ctrl-X" */
+			snprintf(buf, sizeof(buf), "Ctrl-%c", name[1]);
+			return buf;
+		}
+
+		if (name[0] == 'M' && name[1] == '-' && name[2] != 0 && name[3] == 0) {
+			/* translate "M-X" to "Alt-X" */
+			snprintf(buf, sizeof(buf), "Alt-%c", name[2]);
+			return buf;
+		}
+	}
 
 	if (key < 256 && IsAlphaNumericASCII(key))
 		snprintf(buf, sizeof(buf), "\'%c\'", key);
