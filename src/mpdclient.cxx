MusicPlayerDaemon/ncmpc

commit d0a32f476395bda0794b3072ad841fa0f5229c34

mpdclient: add a tag whitelist
Ask MPD to omit the tags we're not interested in from responses.

src/mpdclient.cxx
@@ -217,6 +217,21 @@ mpdclient::GetSettingsName() const
 #endif
 }
 
+#ifdef HAVE_TAG_WHITELIST
+
+void
+mpdclient::WhitelistTags(TagMask mask) noexcept
+{
+	if (!enable_tag_whitelist) {
+		enable_tag_whitelist = true;
+		tag_whitelist = TagMask::None();
+	}
+
+	tag_whitelist |= mask;
+}
+
+#endif
+
 void
 mpdclient::ClearStatus() noexcept
 {
@@ -264,6 +279,32 @@ mpdclient::Disconnect()
 	events |= MPD_IDLE_ALL;
 }
 
+#ifdef HAVE_TAG_WHITELIST
+
+static bool
+SendTagWhitelist(struct mpd_connection *c, const TagMask whitelist) noexcept
+{
+	if (!mpd_command_list_begin(c, false) ||
+	    !mpd_send_clear_tag_types(c))
+		return false;
+
+	/* convert the "tag_bits" mask to an array of enum
+	   mpd_tag_type for mpd_send_enable_tag_types() */
+
+	enum mpd_tag_type types[64];
+	unsigned n = 0;
+
+	for (unsigned i = 0; i < MPD_TAG_COUNT; ++i)
+		if (whitelist.Test((enum mpd_tag_type)i))
+			types[n++] = (enum mpd_tag_type)i;
+
+	return (n == 0 || mpd_send_enable_tag_types(c, types, n)) &&
+		mpd_command_list_end(c) &&
+		mpd_response_finish(c);
+}
+
+#endif
+
 bool
 mpdclient::OnConnected(struct mpd_connection *_connection) noexcept
 {
@@ -289,6 +330,16 @@ mpdclient::OnConnected(struct mpd_connection *_connection) noexcept
 		mpdclient_failed_callback();
 		return false;
 	}
+
+#ifdef HAVE_TAG_WHITELIST
+	if (enable_tag_whitelist &&
+	    !SendTagWhitelist(connection, tag_whitelist)) {
+		InvokeErrorCallback();
+		Disconnect();
+		mpdclient_failed_callback();
+		return false;
+	}
+#endif
 
 	source = new MpdIdleSource(get_io_service(), *connection, *this);
 	ScheduleEnterIdle();

src/Instance.cxx
@@ -22,6 +22,27 @@
 
 #include <signal.h>
 
+#ifdef HAVE_TAG_WHITELIST
+
+#include "strfsong.hxx"
+#include "TagMask.hxx"
+
+static constexpr TagMask global_tag_whitelist{
+	/* these tags are used by SongPage.cxx */
+	MPD_TAG_ARTIST,
+	MPD_TAG_TITLE,
+	MPD_TAG_ALBUM,
+	MPD_TAG_COMPOSER,
+	MPD_TAG_NAME,
+	MPD_TAG_DISC,
+	MPD_TAG_TRACK,
+	MPD_TAG_DATE,
+	MPD_TAG_GENRE,
+	MPD_TAG_COMMENT,
+};
+
+#endif
+
 Instance::Instance()
 	:io_service(),
 #ifndef _WIN32
@@ -53,6 +74,18 @@ Instance::Instance()
 
 #ifndef _WIN32
 	AsyncWaitSigwinch();
+#endif
+
+#ifdef HAVE_TAG_WHITELIST
+	TagMask tag_mask = global_tag_whitelist;
+	tag_mask |= SongFormatToTagMask(options.list_format.c_str());
+	tag_mask |= SongFormatToTagMask(options.search_format.c_str());
+	tag_mask |= SongFormatToTagMask(options.status_format.c_str());
+#ifndef NCMPC_MINI
+	tag_mask |= SongFormatToTagMask(options.xterm_title_format.c_str());
+#endif
+
+	client.WhitelistTags(tag_mask);
 #endif
 }
 

src/TagMask.hxx
@@ -1,0 +1,105 @@
+/* ncmpc (Ncurses MPD Client)
+ * (c) 2004-2019 The Music Player Daemon Project
+ * Project homepage: http://musicpd.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef TAG_MASK_HXX
+#define TAG_MASK_HXX
+
+#include <mpd/tag.h>
+
+#include <initializer_list>
+
+#include <stdint.h>
+
+class TagMask {
+	typedef uint_least32_t mask_t;
+	mask_t value;
+
+	explicit constexpr TagMask(uint_least32_t _value) noexcept
+		:value(_value) {}
+
+public:
+	TagMask() = default;
+
+	constexpr TagMask(enum mpd_tag_type tag) noexcept
+		:value(mask_t(1) << mask_t(tag)) {}
+
+	constexpr TagMask(std::initializer_list<enum mpd_tag_type> il) noexcept
+		:value(0)
+	{
+		for (auto i : il)
+			*this |= TagMask(i);
+	}
+
+	static constexpr TagMask None() noexcept {
+		return TagMask(mask_t(0));
+	}
+
+	static constexpr TagMask All() noexcept {
+		return ~None();
+	}
+
+	constexpr TagMask operator~() const noexcept {
+		return TagMask(~value);
+	}
+
+	constexpr TagMask operator&(TagMask other) const noexcept {
+		return TagMask(value & other.value);
+	}
+
+	constexpr TagMask operator|(TagMask other) const noexcept {
+		return TagMask(value | other.value);
+	}
+
+	constexpr TagMask operator^(TagMask other) const noexcept {
+		return TagMask(value ^ other.value);
+	}
+
+	constexpr TagMask &operator&=(TagMask other) noexcept {
+		value &= other.value;
+		return *this;
+	}
+
+	constexpr TagMask &operator|=(TagMask other) noexcept {
+		value |= other.value;
+		return *this;
+	}
+
+	constexpr TagMask &operator^=(TagMask other) noexcept {
+		value ^= other.value;
+		return *this;
+	}
+
+	constexpr bool TestAny() const noexcept {
+		return value != 0;
+	}
+
+	constexpr bool Test(enum mpd_tag_type tag) const noexcept {
+		return (*this & tag).TestAny();
+	}
+
+	void Set(enum mpd_tag_type tag) noexcept {
+		*this |= tag;
+	}
+
+	void Unset(enum mpd_tag_type tag) noexcept {
+		*this |= ~TagMask(tag);
+	}
+};
+
+#endif

src/mpdclient.hxx
@@ -32,6 +32,11 @@
 
 #include <mpd/client.h> // IWYU pragma: export
 
+#if LIBMPDCLIENT_CHECK_VERSION(2,12,0)
+#define HAVE_TAG_WHITELIST
+#include "TagMask.hxx"
+#endif
+
 #include <boost/asio/steady_timer.hpp>
 
 #include <string>
@@ -112,6 +117,11 @@ struct mpdclient final
 
 	enum mpd_state state = MPD_STATE_UNKNOWN;
 
+#ifdef HAVE_TAG_WHITELIST
+	bool enable_tag_whitelist = false;
+	TagMask tag_whitelist;
+#endif
+
 #if defined(ENABLE_ASYNC_CONNECT) && !defined(_WIN32)
 	bool connecting2;
 #endif
@@ -178,6 +188,10 @@ struct mpdclient final
 	 * by the caller
 	 */
 	std::string GetSettingsName() const;
+
+#ifdef HAVE_TAG_WHITELIST
+	void WhitelistTags(TagMask mask) noexcept;
+#endif
 
 	bool IsConnected() const {
 		return connection != nullptr;

src/strfsong.cxx
@@ -21,6 +21,7 @@
 #include "charset.hxx"
 #include "time_format.hxx"
 #include "util/UriUtil.hxx"
+#include "TagMask.hxx"
 
 #include <mpd/client.h>
 
@@ -278,4 +279,29 @@ strfsong(char *s, size_t max, const char *format,
 	 const struct mpd_song *song) noexcept
 {
 	return _strfsong(s, s + max, format, song, nullptr);
+}
+
+TagMask
+SongFormatToTagMask(const char *format) noexcept
+{
+	TagMask mask = TagMask::None();
+
+	/* TODO: this is incomplete and not correct; the missing tags
+	   are already in global_tag_whitelist (see Instance.cxx); but
+	   for now, this implementation may be good enough */
+
+	static constexpr struct {
+		const char *s;
+		enum mpd_tag_type tag;
+	} tag_references[] = {
+		{"%albumartist%", MPD_TAG_ALBUM_ARTIST},
+		{"%composer%", MPD_TAG_COMPOSER},
+		{"%performer%", MPD_TAG_PERFORMER},
+	};
+
+	for (const auto &i : tag_references)
+		if (strstr(format, i.s) != nullptr)
+			mask |= i.tag;
+
+	return mask;
 }

src/strfsong.hxx
@@ -20,12 +20,22 @@
 #ifndef STRFSONG_H
 #define STRFSONG_H
 
+#include "util/Compiler.h"
+
 #include <stddef.h>
 
 struct mpd_song;
+class TagMask;
 
 size_t
 strfsong(char *s, size_t max, const char *format,
 	 const struct mpd_song *song) noexcept;
+
+/**
+ * Check which tags are referenced by the given song format.
+ */
+gcc_pure
+TagMask
+SongFormatToTagMask(const char *format) noexcept;
 
 #endif

commit 74f68a0265417c332087842dc6f9deab9e17e1ec

mpdclient: send tag whitelist only to MPD 0.21 or newer
This feature requires MPD 0.21, and commit d0a32f4763 broke
compatibility with older versions.

Closes https://github.com/MusicPlayerDaemon/ncmpc/issues/62

src/mpdclient.cxx
@@ -333,6 +333,7 @@ mpdclient::OnConnected(struct mpd_connection *_connection) noexcept
 
 #ifdef HAVE_TAG_WHITELIST
 	if (enable_tag_whitelist &&
+	    mpd_connection_cmp_server_version(connection, 0, 21, 0) >= 0 &&
 	    !SendTagWhitelist(connection, tag_whitelist)) {
 		InvokeErrorCallback();
 		Disconnect();