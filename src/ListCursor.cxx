MusicPlayerDaemon/ncmpc

commit db6e2e0cb6880d687ce468093fe85d6ee26aa004

ListCursor: range-check scroll_offset in FetchCursor()
Closes https://github.com/MusicPlayerDaemon/ncmpc/issues/72

src/ListCursor.cxx
@@ -131,8 +131,11 @@ ListCursor::MoveCursor(unsigned n) noexcept
 void
 ListCursor::FetchCursor() noexcept
 {
-	if (start > 0 &&
-	    selected < start + options.scroll_offset)
+	if (options.scroll_offset * 2 >= GetHeight())
+		// Center if the offset is more than half the screen
+		MoveCursor(start + GetHeight() / 2);
+	else if (start > 0 &&
+		 selected < start + options.scroll_offset)
 		MoveCursor(start + options.scroll_offset);
 	else if (start + GetHeight() < length &&
 		 selected > start + GetHeight() - 1 - options.scroll_offset)

commit 2a60a9f3937ddef6a1ed1f00871f5ea622e5faf3

ListCursor: clamp scroll_offset, replacing special case
This replaces commit db6e2e0cb6880d687ce468093fe85d6ee26aa004, which
caused a jumping cursor on song change when the whole queue was
visible.

src/ListCursor.cxx
@@ -131,15 +131,18 @@ ListCursor::MoveCursor(unsigned n) noexcept
 void
 ListCursor::FetchCursor() noexcept
 {
-	if (options.scroll_offset * 2 >= GetHeight())
-		// Center if the offset is more than half the screen
-		MoveCursor(start + GetHeight() / 2);
-	else if (start > 0 &&
-		 selected < start + options.scroll_offset)
-		MoveCursor(start + options.scroll_offset);
+	/* clamp the scroll-offset setting to slightly less than half
+	   of the screen height */
+	const unsigned scroll_offset = options.scroll_offset * 2 < GetHeight()
+		? options.scroll_offset
+		: std::max(GetHeight() / 2, 1U) - 1;
+
+	if (start > 0 &&
+	    selected < start + scroll_offset)
+		MoveCursor(start + scroll_offset);
 	else if (start + GetHeight() < length &&
-		 selected > start + GetHeight() - 1 - options.scroll_offset)
-		MoveCursor(start + GetHeight() - 1 - options.scroll_offset);
+		 selected > start + GetHeight() - 1 - scroll_offset)
+		MoveCursor(start + GetHeight() - 1 - scroll_offset);
 }
 
 ListWindowRange