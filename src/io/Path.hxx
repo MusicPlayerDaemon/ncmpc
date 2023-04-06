c790e1a63a20c74509a3549eecea3b3a2a67052b
Max Kellermann <max@musicpd.org>
2023-04-06 12:34:23 +0200

    io/Path: cast parameters to std::string_view

--- src/io/Path.hxx
+++ src/io/Path.hxx
@@ -5,8 +5,7 @@
 #define IO_PATH_HXX
 
 #include <string>
-
-#include <string.h>
+#include <string_view>
 
 namespace PathDetail {
 
@@ -16,93 +15,37 @@ static constexpr char SEPARATOR = '\\';
 static constexpr char SEPARATOR = '/';
 #endif
 
-[[gnu::pure]]
-inline size_t
-GetLength(const char *s) noexcept
-{
-	return strlen(s);
-}
-
-[[gnu::pure]]
-inline size_t
-GetLength(const std::string &s) noexcept
-{
-	return s.length();
-}
-
-template<typename... Args>
-inline size_t
-FillLengths(size_t *lengths, Args&&... args) noexcept;
-
-template<typename First, typename... Args>
-inline size_t
-FillLengths(size_t *lengths, First &&first, Args&&... args) noexcept
-{
-	size_t length = GetLength(std::forward<First>(first));
-	*lengths++ = length;
-	return length + FillLengths(lengths, std::forward<Args>(args)...);
-}
-
-template<>
-inline size_t
-FillLengths(size_t *) noexcept
-{
-	return 0;
-}
-
-inline std::string &
-Append(std::string &dest, const std::string &value, size_t length) noexcept
-{
-	return dest.append(value, 0, length);
-}
-
-inline std::string &
-Append(std::string &dest, const char *value, size_t length) noexcept
-{
-	return dest.append(value, length);
-}
-
-template<typename... Args>
-inline std::string &
-AppendWithSeparators(std::string &dest, const size_t *lengths,
-		     Args&&... args) noexcept;
-
-template<typename First, typename... Args>
-inline std::string &
-AppendWithSeparators(std::string &dest, const size_t *lengths,
-		     First &&first, Args&&... args) noexcept
+inline void
+AppendWithSeparator(std::string &dest, std::string_view src) noexcept
 {
 	dest.push_back(SEPARATOR);
-	return AppendWithSeparators(Append(dest, std::forward<First>(first),
-					   *lengths),
-				    lengths + 1,
-				    std::forward<Args>(args)...);
+	dest.append(src);
 }
 
-template<>
-inline std::string &
-AppendWithSeparators(std::string &dest, const size_t *) noexcept
+template<typename... Args>
+std::string
+BuildPath(std::string_view first, Args&&... args) noexcept
 {
-	return dest;
+	constexpr size_t n = sizeof...(args);
+
+	const std::size_t total = first.size() + (args.size() + ...);
+
+	std::string result;
+	result.reserve(total + n);
+
+	result.append(first);
+	(AppendWithSeparator(result, args), ...);
+
+	return result;
 }
 
 } // namespace PathDetail
 
-template<typename First, typename... Args>
+template<typename... Args>
 std::string
-BuildPath(First &&first, Args&&... args) noexcept
+BuildPath(std::string_view first, Args&&... args) noexcept
 {
-	constexpr size_t n = sizeof...(args);
-
-	size_t lengths[n + 1];
-	const size_t total = PathDetail::FillLengths(lengths, first, args...);
-
-	std::string result;
-	result.reserve(total + n);
-	PathDetail::Append(result, std::forward<First>(first), lengths[0]);
-	PathDetail::AppendWithSeparators(result, lengths + 1,
-					 std::forward<Args>(args)...);
-	return result;
+	return PathDetail::BuildPath(first, static_cast<std::string_view>(args)...);
 }
 
 #endif
