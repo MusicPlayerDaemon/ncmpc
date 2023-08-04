c13e7d64f3cd5d24afe6fc6369c7655cba005a5a
Max Kellermann <max@musicpd.org>
2023-08-04 13:02:44 +0200

    system/Error: remove unused functions

--- src/system/Error.hxx
+++ src/system/Error.hxx
@@ -6,23 +6,10 @@
 #include <system_error> // IWYU pragma: export
 #include <utility>
 
-#include <stdio.h>
-
-template<typename... Args>
-static inline std::system_error
-FormatSystemError(std::error_code code, const char *fmt,
-		  Args&&... args) noexcept
-{
-	char buffer[1024];
-	snprintf(buffer, sizeof(buffer), fmt, std::forward<Args>(args)...);
-	return std::system_error(code, buffer);
-}
-
 #ifdef _WIN32
 
 #include <errhandlingapi.h> // for GetLastError()
-#include <windef.h> // for HWND (needed by winbase.h)
-#include <winbase.h> // for FormatMessageA()
+#include <winerror.h>
 
 /**
  * Returns the error_category to be used to wrap WIN32 GetLastError()
@@ -59,37 +46,9 @@ MakeLastError(const char *msg) noexcept
 	return MakeLastError(GetLastError(), msg);
 }
 
-template<typename... Args>
-static inline std::system_error
-FormatLastError(DWORD code, const char *fmt, Args&&... args) noexcept
-{
-	char buffer[512];
-	const auto end = buffer + sizeof(buffer);
-	size_t length = snprintf(buffer, sizeof(buffer) - 128,
-				 fmt, std::forward<Args>(args)...);
-	char *p = buffer + length;
-	*p++ = ':';
-	*p++ = ' ';
-
-	FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM |
-		       FORMAT_MESSAGE_IGNORE_INSERTS,
-		       nullptr, code, 0, p, end - p, nullptr);
-	return MakeLastError(code, buffer);
-}
-
-template<typename... Args>
-static inline std::system_error
-FormatLastError(const char *fmt, Args&&... args) noexcept
-{
-	return FormatLastError(GetLastError(), fmt,
-			       std::forward<Args>(args)...);
-}
-
 #endif /* _WIN32 */
 
 #include <cerrno> // IWYU pragma: export
-
-#include <string.h>
 
 /**
  * Returns the error_category to be used to wrap errno values.  The
@@ -125,35 +84,6 @@ static inline std::system_error
 MakeErrno(const char *msg) noexcept
 {
 	return MakeErrno(errno, msg);
-}
-
-template<typename... Args>
-static inline std::system_error
-FormatErrno(int code, const char *fmt, Args&&... args) noexcept
-{
-	char buffer[512];
-	snprintf(buffer, sizeof(buffer),
-		 fmt, std::forward<Args>(args)...);
-	return MakeErrno(code, buffer);
-}
-
-template<typename... Args>
-static inline std::system_error
-FormatErrno(const char *fmt, Args&&... args) noexcept
-{
-	return FormatErrno(errno, fmt, std::forward<Args>(args)...);
-}
-
-template<typename... Args>
-static inline std::system_error
-FormatFileNotFound(const char *fmt, Args&&... args) noexcept
-{
-#ifdef _WIN32
-	return FormatLastError(ERROR_FILE_NOT_FOUND, fmt,
-			       std::forward<Args>(args)...);
-#else
-	return FormatErrno(ENOENT, fmt, std::forward<Args>(args)...);
-#endif
 }
 
 [[gnu::pure]]
