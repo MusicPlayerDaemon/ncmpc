51bc029c940bb5121242f4b9f9a8f93ea93030f8
Max Kellermann <max@musicpd.org>
2024-09-09 20:58:56 +0200

    GlobalBindings: remove keycode shortcut macros
    These are somewhat obscure.  Let's be more verbose and readable.

--- src/GlobalBindings.cxx
+++ src/GlobalBindings.cxx
@@ -8,72 +8,51 @@
 
 #include <curses.h>
 
-#define BS   KEY_BACKSPACE
-#define DEL  KEY_DC
-#define UP   KEY_UP
-#define DWN  KEY_DOWN
-#define LEFT KEY_LEFT
-#define RGHT KEY_RIGHT
-#define HOME KEY_HOME
-#define END  KEY_END
-#define PGDN KEY_NPAGE
-#define PGUP KEY_PPAGE
-#define STAB 0x161
 #define ESC  0x1B
 #define RET  '\r'
-#define F1   KEY_F(1)
-#define F2   KEY_F(2)
-#define F3   KEY_F(3)
-#define F4   KEY_F(4)
-#define F5   KEY_F(5)
-#define F6   KEY_F(6)
-#define F7   KEY_F(7)
-#define F8   KEY_F(8)
-#define F9   KEY_F(9)
-#define C(x) KEY_CTL(x)
 
 static KeyBindings global_key_bindings{{{
 #ifdef ENABLE_KEYDEF_SCREEN
 	{'K'},
 #endif
-	{'q', 'Q', C('C')},
+	{'q', 'Q', KEY_CTL('C')},
 
 	/* movement */
-	{UP, 'k'},
-	{DWN, 'j'},
+	{KEY_UP, 'k'},
+	{KEY_DOWN, 'j'},
 	{'H'},
 	{'M'},
 	{'L'},
-	{HOME, C('A')},
-	{END, C('E')},
-	{PGUP},
-	{PGDN},
+	{KEY_HOME, KEY_CTL('A')},
+	{KEY_END, KEY_CTL('E')},
+	{KEY_NPAGE},
+	{KEY_PPAGE},
 	{'v',  0},
-	{C('N'),  0},
-	{C('B'),  0},
+	{KEY_CTL('N'),  0},
+	{KEY_CTL('B'),  0},
 	{'N',  0},
 	{'B',  0},
 	{'l'},
 
 	/* basic screens */
-	{'1', F1, 'h'},
-	{'2', F2},
-	{'3', F3},
+	{'1', KEY_F(1), 'h'},
+	{'2', KEY_F(2)},
+	{'3', KEY_F(3)},
 
 	/* player commands */
 	{RET},
 	{'P'},
-	{'s', BS},
+	{'s', KEY_BACKSPACE},
 	{'o'},
 	{'>'},
 	{'<'},
 	{'f'},
 	{'b'},
-	{'+', RGHT},
-	{'-', LEFT},
+	{'+', KEY_RIGHT},
+	{'-', KEY_LEFT},
 	{' '},
 	{'t'},
-	{DEL, 'd'},
+	{KEY_DC, 'd'},
 	{'Z'},
 	{'c'},
 	{'r'},
@@ -81,7 +60,7 @@ static KeyBindings global_key_bindings{{{
 	{'y'},
 	{'C'},
 	{'x'},
-	{C('U')},
+	{KEY_CTL('U')},
 	{'S'},
 	{'a'},
 
@@ -91,9 +70,9 @@ static KeyBindings global_key_bindings{{{
 	{'G'},
 
 	/* lists */
-	{C('K')},
-	{C('J')},
-	{C('L')},
+	{KEY_CTL('K')},
+	{KEY_CTL('J')},
+	{KEY_CTL('L')},
 
 
 	/* ncmpc options */
@@ -102,7 +81,7 @@ static KeyBindings global_key_bindings{{{
 
 	/* change screen */
 	{TAB},
-	{STAB},
+	{KEY_STAB},
 	{'`'},
 
 
@@ -116,20 +95,20 @@ static KeyBindings global_key_bindings{{{
 
 	/* extra screens */
 #ifdef ENABLE_LIBRARY_PAGE
-	{'4', F4},
+	{'4', KEY_F(4)},
 #endif
 #ifdef ENABLE_SEARCH_SCREEN
-	{'5', F5},
+	{'5', KEY_F(5)},
 	{'m'},
 #endif
 #ifdef ENABLE_PLAYLIST_EDITOR
-	{C('P')},
+	{KEY_CTL('P')},
 #endif
 #ifdef ENABLE_SONG_SCREEN
 	{'i'},
 #endif
 #ifdef ENABLE_LYRICS_SCREEN
-	{'7', F7},
+	{'7', KEY_F(7)},
 	{ESC},
 	{'u'},
 #endif
@@ -138,11 +117,11 @@ static KeyBindings global_key_bindings{{{
 #endif
 
 #ifdef ENABLE_OUTPUTS_SCREEN
-	{'8', F8},
+	{'8', KEY_F(8)},
 #endif
 
 #ifdef ENABLE_CHAT_SCREEN
-	{'9', F9},
+	{'9', KEY_F(9)},
 #endif
 }}};
 
