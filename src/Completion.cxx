58cfb5b6f2bd45c1da32917b4072373d37d452d3
Max Kellermann <max@musicpd.org>
2024-09-11 16:31:38 +0200

    Completion: pass std::string::starts_with()

--- src/Completion.cxx
+++ src/Completion.cxx
@@ -5,22 +5,15 @@
 
 #include <assert.h>
 
-static bool
-StartsWith(const std::string &haystack, const std::string &needle) noexcept
-{
-	return haystack.length() >= needle.length() &&
-		std::equal(needle.begin(), needle.end(), haystack.begin());
-}
-
 Completion::Result
 Completion::Complete(const std::string &prefix) const noexcept
 {
 	auto lower = list.lower_bound(prefix);
-	if (lower == list.end() || !StartsWith(*lower, prefix))
+	if (lower == list.end() || !lower->starts_with(prefix))
 		return {std::string(), {lower, lower}};
 
 	auto upper = list.upper_bound(prefix);
-	while (upper != list.end() && StartsWith(*upper, prefix))
+	while (upper != list.end() && upper->starts_with(prefix))
 		++upper;
 
 	assert(upper != lower);
