af33e8864e39429f7a09d060a374f4e915c063c7
Max Kellermann <max@musicpd.org>
2026-08-20 18:16:13 +0200

    TabBar: show only pages with keys F1..F10 and 1..9,0
    Only the "main" pages are displayed in the title bar.  This prepares
    for adding many more pages which would clutter the title bar.

--- src/TabBar.cxx
+++ src/TabBar.cxx
@@ -43,11 +43,48 @@ TabBar::Paint(const Window window, const PageMeta &current_page_meta,
 {
 	const auto &key_bindings = GetGlobalKeyBindings();
 
-	tabs.clear();
-
-	unsigned x = 0;
-	for (const PageMeta *const*i = all_pages; *i != nullptr && !tabs.full(); ++i) {
+	/* collect all pages with hot keys F1..F10 or 1..9,0; these
+	   are the "main" pages that shall be displayed always */
+	std::array<const PageMeta *, 10> n_pages{}, f_pages{};
+	for (const PageMeta *const*i = all_pages; *i != nullptr; ++i) {
 		const auto &page = **i;
+		const auto &key_binding = key_bindings.Get(page.command);
+		for (auto key : key_binding.keys) {
+			if (key >= '1' && key <= '9')
+				n_pages[key - '1'] = &page;
+			else if (key == '0')
+				n_pages.back() = &page;
+			else if (key >= KEY_F(1) && key <= KEY_F(10))
+				n_pages[key - KEY_F(1)] = &page;
+		}
+	}
+
+	/* combine the sparse arrays into a single array and append
+	   the current page (unless it's already visible */
+	StaticVector<const PageMeta *, 16> pages;
+	bool found_current = false;
+	for (unsigned i = 0; i < 10; ++i) {
+		const auto *page = n_pages[i];
+		if (page == nullptr) {
+			page = f_pages[i];
+			if (page == nullptr)
+				continue;
+		}
+
+		if (page == &current_page_meta)
+			found_current = true;
+
+		pages.emplace_back(page);
+	}
+
+	if (!found_current)
+		pages.emplace_back(&current_page_meta);
+
+	/* now draw the pages we collected */
+	tabs.clear();
+	unsigned x = 0;
+	for (const auto *i : pages) {
+		const auto &page = *i;
 
 		std::string_view title{};
 		if (&page == &current_page_meta)
