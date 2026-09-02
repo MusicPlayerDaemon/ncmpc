MusicPlayerDaemon/ncmpc

commit 29c9c8c305954dae264e361c7d55c07516a2e21a

FileListPage: embed the FileList directly

src/FileListPage.cxx
@@ -68,10 +68,9 @@ std::string_view
 FileListPage::GetListItemText(std::span<char> buffer,
 			      unsigned idx) const noexcept
 {
-	assert(filelist != nullptr);
-	assert(idx < filelist->size());
+	assert(idx < filelist.size());
 
-	const auto &entry = (*filelist)[idx];
+	const auto &entry = filelist[idx];
 	const auto *entity = entry.entity;
 
 	if( entity == nullptr )
@@ -157,18 +156,30 @@ FileListPage::EnqueueAndPlay(struct mpdclient &c, FileListEntry &entry) noexcept
 	return true;
 }
 
-FileListEntry *
+inline FileListEntry *
+FileListPage::GetSelectedEntry() noexcept
+{
+	const auto range = lw.GetRange();
+
+	if (range.empty() ||
+	    range.end_index > range.start_index + 1 ||
+	    range.start_index >= filelist.size())
+		return nullptr;
+
+	return &filelist[range.start_index];
+}
+
+inline const FileListEntry *
 FileListPage::GetSelectedEntry() const noexcept
 {
 	const auto range = lw.GetRange();
 
-	if (filelist == nullptr ||
-	    range.empty() ||
+	if (range.empty() ||
 	    range.end_index > range.start_index + 1 ||
-	    range.start_index >= filelist->size())
+	    range.start_index >= filelist.size())
 		return nullptr;
 
-	return &(*filelist)[range.start_index];
+	return &filelist[range.start_index];
 }
 
 const struct mpd_entity *
@@ -192,13 +203,13 @@ FileListPage::GetSelectedSong() const noexcept
 		: nullptr;
 }
 
-FileListEntry *
-FileListPage::GetIndex(unsigned i) const noexcept
+inline FileListEntry *
+FileListPage::GetIndex(unsigned i) noexcept
 {
-	if (filelist == nullptr || i >= filelist->size())
+	if (i >= filelist.size())
 		return nullptr;
 
-	return &(*filelist)[i];
+	return &filelist[i];
 }
 
 bool
@@ -328,11 +339,8 @@ FileListPage::HandleEdit(struct mpdclient &c) noexcept
 inline void
 FileListPage::HandleSelectAll(struct mpdclient &c) noexcept
 {
-	if (filelist == nullptr)
-		return;
-
-	for (unsigned i = 0; i < filelist->size(); ++i) {
-		auto &entry = (*filelist)[i];
+	for (unsigned i = 0; i < filelist.size(); ++i) {
+		auto &entry = filelist[i];
 
 		if (entry.entity != nullptr)
 			HandleSelectEntry(c, entry, false);
@@ -374,9 +382,6 @@ FileListPage::OnMouse(struct mpdclient &c, Point p,
 bool
 FileListPage::OnCommand(struct mpdclient &c, Command cmd)
 {
-	if (filelist == nullptr)
-		return false;
-
 	if (ListPage::OnCommand(c, cmd))
 		return true;
 
@@ -459,10 +464,9 @@ FileListPage::PaintListItem(const Window window, unsigned i,
 			    unsigned y, unsigned width,
 			    bool selected) const noexcept
 {
-	assert(filelist != nullptr);
-	assert(i < filelist->size());
+	assert(i < filelist.size());
 
-	const auto &entry = (*filelist)[i];
+	const auto &entry = filelist[i];
 	const struct mpd_entity *entity = entry.entity;
 	if (entity == nullptr) {
 		screen_browser_paint_directory(window, width, selected, ".."sv);
@@ -520,10 +524,9 @@ FileListPage::PaintStatusBarOverride(const Window window) const noexcept
 
 	unsigned duration = 0;
 
-	assert(filelist != nullptr);
 	for (const unsigned i : lw.GetRange()) {
-		assert(i < filelist->size());
-		const auto &entry = (*filelist)[i];
+		assert(i < filelist.size());
+		const auto &entry = filelist[i];
 		const auto *entity = entry.entity;
 
 		if (entity != nullptr &&

src/EditPlaylistPage.cxx
@@ -66,12 +66,12 @@ LoadPlaylist(struct mpdclient &c, const char *name, FileList &l)
 	return c.FinishCommand();
 }
 
-static std::unique_ptr<FileList>
+static FileList
 LoadPlaylist(struct mpdclient &c, const std::string &name)
 {
-	auto l = std::make_unique<FileList>();
+	FileList l;
 	if (!name.empty())
-		LoadPlaylist(c, name.c_str(), *l);
+		LoadPlaylist(c, name.c_str(), l);
 
 	return l;
 }
@@ -80,7 +80,7 @@ void
 EditPlaylistPage::Reload(struct mpdclient &c)
 {
 	filelist = ::LoadPlaylist(c, name.c_str());
-	lw.SetLength(filelist->size());
+	lw.SetLength(filelist.size());
 	SchedulePaint();
 }
 
@@ -96,11 +96,11 @@ EditPlaylistPage::SaveSelection() noexcept
 void
 EditPlaylistPage::RestoreSelection() noexcept
 {
-	if (!filelist || !selected_song)
+	if (!selected_song)
 		/* there was no selection */
 		return;
 
-	int i = filelist->FindSong(*selected_song);
+	int i = filelist.FindSong(*selected_song);
 	if (i >= 0)
 		lw.SetCursor(i);
 

src/FileBrowserPage.cxx
@@ -96,14 +96,14 @@ screen_file_load_list(struct mpdclient *c, const char *current_path,
 bool
 FileBrowserPage::Reload(struct mpdclient &c) noexcept
 {
-	filelist = std::make_unique<FileList>();
+	filelist.clear();
 	if (!current_path.empty())
 		/* add a dummy entry for ./.. */
-		filelist->emplace_back(nullptr);
+		filelist.emplace_back(nullptr);
 
-	const bool success = screen_file_load_list(&c, current_path.c_str(), *filelist);
+	const bool success = screen_file_load_list(&c, current_path.c_str(), filelist);
 
-	lw.SetLength(filelist->size());
+	lw.SetLength(filelist.size());
 
 	SchedulePaint();
 
@@ -118,7 +118,7 @@ FileBrowserPage::ChangeDirectory(struct mpdclient &c,
 
 	const bool success = Reload(c);
 
-	screen_browser_sync_highlights(*filelist, c.playlist);
+	screen_browser_sync_highlights(filelist, c.playlist);
 
 	lw.Reset();
 
@@ -133,7 +133,7 @@ FileBrowserPage::ChangeToParent(struct mpdclient &c) noexcept
 	bool success = ChangeDirectory(c, GetParentUri(old_path));
 
 	int idx = success
-		? filelist->FindDirectory(old_path.c_str())
+		? filelist.FindDirectory(old_path.c_str())
 		: -1;
 
 	if (success && idx >= 0) {
@@ -176,7 +176,7 @@ FileBrowserPage::GotoSong(struct mpdclient &c, const struct mpd_song &song) noex
 
 	/* select the specified song */
 
-	int i = filelist->FindSong(song);
+	int i = filelist.FindSong(song);
 	if (i < 0)
 		i = 0;
 
@@ -205,7 +205,7 @@ FileBrowserPage::HandleSave(struct mpdclient &c) noexcept
 		return;
 
 	for (const unsigned i : range) {
-		auto &entry = (*filelist)[i];
+		auto &entry = filelist[i];
 		if (entry.entity) {
 			struct mpd_entity *entity = entry.entity;
 			if (mpd_entity_get_type(entity) == MPD_ENTITY_TYPE_PLAYLIST) {
@@ -232,7 +232,7 @@ FileBrowserPage::HandleDelete(struct mpdclient &c)
 	std::forward_list<std::string> playlists;
 	const auto range = lw.GetRange();
 	for (auto it = playlists.before_begin(); const unsigned i : range) {
-		auto &entry = (*filelist)[i];
+		auto &entry = filelist[i];
 		if (entry.entity == nullptr)
 			continue;
 
@@ -314,7 +314,7 @@ FileBrowserPage::Update(struct mpdclient &c, unsigned events) noexcept
 		      | MPD_IDLE_QUEUE
 #endif
 		      )) {
-		screen_browser_sync_highlights(*filelist, c.playlist);
+		screen_browser_sync_highlights(filelist, c.playlist);
 		SchedulePaint();
 	}
 }
@@ -334,7 +334,7 @@ FileBrowserPage::OnCommand(struct mpdclient &c, Command cmd)
 
 	case Command::SCREEN_UPDATE:
 		Reload(c);
-		screen_browser_sync_highlights(*filelist, c.playlist);
+		screen_browser_sync_highlights(filelist, c.playlist);
 		return false;
 
 	default:

src/FileListPage.hxx
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "config.h"
+#include "filelist.hxx"
 #include "page/ListPage.hxx"
 #include "ui/ListRenderer.hxx"
 #include "ui/ListText.hxx"
@@ -13,14 +14,12 @@
 struct mpdclient;
 struct MpdQueue;
 class ScreenManager;
-class FileList;
-struct FileListEntry;
 
 class FileListPage : public ListPage, ListRenderer, ListText {
 protected:
 	ScreenManager &screen;
 
-	std::unique_ptr<FileList> filelist;
+	FileList filelist;
 	const char *const song_format;
 
 public:
@@ -31,13 +30,16 @@ public:
 
 protected:
 	[[gnu::pure]]
-	FileListEntry *GetSelectedEntry() const noexcept;
+	FileListEntry *GetSelectedEntry() noexcept;
+
+	[[gnu::pure]]
+	const FileListEntry *GetSelectedEntry() const noexcept;
 
 	[[gnu::pure]]
 	const struct mpd_entity *GetSelectedEntity() const noexcept;
 
 	[[gnu::pure]]
-	FileListEntry *GetIndex(unsigned i) const noexcept;
+	FileListEntry *GetIndex(unsigned i) noexcept;
 
 protected:
 	virtual bool HandleEnter(struct mpdclient &c);

src/LibraryPage.cxx
@@ -155,23 +155,23 @@ SongListPage::LoadSongList(struct mpdclient &c)
 {
 	auto *connection = c.GetConnection();
 
-	filelist = std::make_unique<FileList>();
+	filelist.clear();
 	/* add a dummy entry for ".." */
-	filelist->emplace_back(nullptr);
+	filelist.emplace_back(nullptr);
 
 	if (connection != nullptr) {
 		mpd_search_db_songs(connection, true);
 		AddConstraints(connection, filter);
 		mpd_search_commit(connection);
 
-		filelist->Receive(*connection);
+		filelist.Receive(*connection);
 
 		c.FinishCommand();
 	}
 
 	/* fix highlights */
-	screen_browser_sync_highlights(*filelist, c.playlist);
-	lw.SetLength(filelist->size());
+	screen_browser_sync_highlights(filelist, c.playlist);
+	lw.SetLength(filelist.size());
 }
 
 void

src/SearchPage.cxx
@@ -156,17 +156,16 @@ public:
 void
 SearchPage::Clear(bool clear_pattern) noexcept
 {
-	if (filelist) {
-		filelist = std::make_unique<FileList>();
-		lw.SetLength(0);
-	}
+	filelist.clear();
+	lw.SetLength(0);
+
 	if (clear_pattern)
 		pattern.clear();
 
 	SchedulePaint();
 }
 
-static std::unique_ptr<FileList>
+static FileList
 search_simple_query(struct mpd_connection *connection, bool exact_match,
 		    int table, const char *local_pattern)
 {
@@ -189,8 +188,9 @@ search_simple_query(struct mpd_connection *connection, bool exact_match,
 
 		mpd_command_list_end(connection);
 
-		auto list = filelist_new_recv(connection);
-		list->RemoveDuplicateSongs();
+		FileList list;
+		list.Receive(*connection);
+		list.RemoveDuplicateSongs();
 		return list;
 	} else if (table == SEARCH_URI) {
 		mpd_search_db_songs(connection, exact_match);
@@ -198,7 +198,9 @@ search_simple_query(struct mpd_connection *connection, bool exact_match,
 					      filter_utf8.c_str());
 		mpd_search_commit(connection);
 
-		return filelist_new_recv(connection);
+		FileList list;
+		list.Receive(*connection);
+		return list;
 	} else {
 		mpd_search_db_songs(connection, exact_match);
 		mpd_search_add_tag_constraint(connection, MPD_OPERATOR_DEFAULT,
@@ -206,7 +208,9 @@ search_simple_query(struct mpd_connection *connection, bool exact_match,
 					      filter_utf8.c_str());
 		mpd_search_commit(connection);
 
-		return filelist_new_recv(connection);
+		FileList list;
+		list.Receive(*connection);
+		return list;
 	}
 }
 
@@ -274,13 +278,14 @@ ParseModifiedSince(const char *s)
  *       Its ugly and MUST be redesigned before the next release!
  *-----------------------------------------------------------------------
  */
-static std::unique_ptr<FileList>
+static bool
 search_advanced_query(Interface &interface,
-		      struct mpd_connection *connection, const char *query)
+		      struct mpd_connection *connection, const char *query,
+		      FileList &fl)
 try {
 	advanced_search_mode = false;
 	if (strchr(query, ':') == nullptr)
-		return nullptr;
+		return false;
 
 	std::string str(query);
 
@@ -313,7 +318,7 @@ try {
 			if (table[n] < 0) {
 				interface.Alert(fmt::format(fmt::runtime(_("Bad search tag {}")),
 							    tabv[n]));
-				return nullptr;
+				return false;
 			}
 
 			++n;
@@ -327,7 +332,7 @@ try {
 	if (matchv[n - 1][0] == '\0') {
 		interface.Alert(fmt::format(fmt::runtime(_("No argument for search tag {}")),
 					    tabv[n - 1]));
-		return nullptr;
+		return false;
 	}
 
 	advanced_search_mode = true;
@@ -360,39 +365,33 @@ try {
 	}
 
 	mpd_search_commit(connection);
-	auto fl = filelist_new_recv(connection);
-	if (!mpd_response_finish(connection))
-		fl.reset();
-
-	return fl;
+	fl.Receive(*connection);
+	return mpd_response_finish(connection);
 } catch (...) {
 	mpd_search_cancel(connection);
 	throw;
 }
 
-static std::unique_ptr<FileList>
+static FileList
 do_search(Interface &interface,
 	  struct mpdclient *c, const char *query)
 {
 	auto *connection = c->GetConnection();
 	if (connection == nullptr)
-		return nullptr;
+		return {};
 
-	auto fl = search_advanced_query(interface, connection, query);
-	if (fl != nullptr)
+	if (FileList fl;
+	    search_advanced_query(interface, connection, query, fl))
 		return fl;
 
 	if (mpd_connection_get_error(connection) != MPD_ERROR_SUCCESS) {
 		c->HandleError();
-		return nullptr;
+		return {};
 	}
 
-	fl = search_simple_query(connection, false,
-				 mode[options.search_mode].table,
-				 query);
-	if (fl == nullptr)
-		c->HandleError();
-	return fl;
+	return search_simple_query(connection, false,
+				   mode[options.search_mode].table,
+				   query);
 }
 
 void
@@ -403,11 +402,9 @@ SearchPage::Reload(struct mpdclient &c)
 
 	lw.ShowCursor();
 	filelist = do_search(GetInterface(), &c, pattern.c_str());
-	if (filelist == nullptr)
-		filelist = std::make_unique<FileList>();
-	lw.SetLength(filelist->size());
+	lw.SetLength(filelist.size());
 
-	screen_browser_sync_highlights(*filelist, c.playlist);
+	screen_browser_sync_highlights(filelist, c.playlist);
 
 	SchedulePaint();
 }
@@ -443,7 +440,7 @@ screen_search_init(ScreenManager &_screen, const Window window)
 void
 SearchPage::Paint() const noexcept
 {
-	if (filelist) {
+	if (!filelist.empty()) {
 		FileListPage::Paint();
 	} else {
 		lw.Paint(TextListRenderer(SearchHelpText()));
@@ -467,8 +464,8 @@ SearchPage::GetTitle(std::span<char> buffer) const noexcept
 void
 SearchPage::Update(struct mpdclient &c, unsigned events) noexcept
 {
-	if (filelist != nullptr && events & MPD_IDLE_QUEUE) {
-		screen_browser_sync_highlights(*filelist, c.playlist);
+	if (events & MPD_IDLE_QUEUE) {
+		screen_browser_sync_highlights(filelist, c.playlist);
 		SchedulePaint();
 	}
 }

commit 7cf8ffc786fc893b81fa7cbe43e0f08361898eb9

FileListPage: remove misplaced `inline`
Caused linker errors; regression by commit
29c9c8c305954dae264e361c7d55c07516a2e21a

Closes https://github.com/MusicPlayerDaemon/ncmpc/issues/169

src/FileListPage.cxx
@@ -156,7 +156,7 @@ FileListPage::EnqueueAndPlay(struct mpdclient &c, FileListEntry &entry) noexcept
 	return true;
 }
 
-inline FileListEntry *
+FileListEntry *
 FileListPage::GetSelectedEntry() noexcept
 {
 	const auto range = lw.GetRange();
@@ -169,7 +169,7 @@ FileListPage::GetSelectedEntry() noexcept
 	return &filelist[range.start_index];
 }
 
-inline const FileListEntry *
+const FileListEntry *
 FileListPage::GetSelectedEntry() const noexcept
 {
 	const auto range = lw.GetRange();