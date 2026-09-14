29e220e0b9806e9acb32be4fcbe4dff78b3f801b
Max Kellermann <max.kellermann@ionos.com>
2026-09-14 14:00:10 +0200

    co/AwaitableHelper: no `noexcept` if TakeValue() can throw

--- src/co/AwaitableHelper.hxx
+++ src/co/AwaitableHelper.hxx
@@ -34,7 +34,7 @@ public:
 		task.continuation = _continuation;
 	}
 
-	decltype(auto) await_resume() noexcept(!rethrow_error) {
+	decltype(auto) await_resume() noexcept(!rethrow_error && noexcept(this->task.TakeValue())) {
 		if constexpr (rethrow_error)
 			if (this->task.error)
 				std::rethrow_exception(this->task.error);
